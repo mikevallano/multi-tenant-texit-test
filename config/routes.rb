8eb56eb20ef9cfcd84f89b9e2f93e9464eed6868
mike vallano <mikevallano@gmail.com>
2015-09-04 13:57:49 -0500

    Cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -22,23 +22,9 @@ Rails.application.routes.draw do
     namespace :admin do
       resources :users
     end
-    # devise_scope :user do
-    #   get '/sign_in' => "devise/sessions#new"
-    #   match '/sign_in', :to => 'devise/sessions#new', via: [:get, :post]
-    #   match '/sign_out', :to => 'devise/sessions#destroy', via: [:get, :delete], as: :destroy_user_session
-    #   match '/edit', :to => 'devise/registrations#edit', via: [:get, :post]
-    #   post  '/password' => "devise/passwords#create"
-    #   get    '/password/new' => "devise/passwords#new"
-    #   get    '/password/edit' => "devise/passwords#edit"
-    #   put    '/password' => "devise/passwords#update"
-    # end
   end
 
   constraints(SubdomainBlank) do
-    # devise_scope :user do
-    #   get '/register' => "devise/sessions#new"
-    #   post  '/register' => 'devise/registrations#new'
-    # end
     root 'pages#home'
     get 'pages/about'
     resources :accounts
