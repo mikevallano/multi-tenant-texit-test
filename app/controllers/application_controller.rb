64be46423675d62cd2597672c6b12d4a1d5ec5c6
mike vallano <mikevallano@gmail.com>
2015-09-07 09:58:29 -0500

    Tweaks to clean up application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,6 @@ class ApplicationController < ActionController::Base
 
   around_filter :scope_current_account
 
-  # def user_in_account
-  #   unless request.subdomain == current_user.account.subdomain
-  #     redirect_to subdomain_root_url, alert: "You are not authorized to access that subdomain."
-  #   end
-  # end
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password,
       :password_confirmation, account_attributes: [:subdomain, :owner_id])}
@@ -24,7 +18,7 @@ class ApplicationController < ActionController::Base
   end
 
   def require_account!
-    redirect_to root_url if !@account.present?
+    redirect_to root_url unless @account.present?
   end
 
   def scope_current_account
